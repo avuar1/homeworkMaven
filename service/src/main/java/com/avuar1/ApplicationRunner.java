20a75b8a16aa0bfb4ec877e95b141bb2a8782866
avuar1 <avuar1@mail.ru>
2023-09-04 18:42:14 +0400

    refactor

--- service/src/main/java/com/avuar1/ApplicationRunner.java
+++ service/src/main/java/com/avuar1/ApplicationRunner.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableJpaRepositories
 public class ApplicationRunner {
     public static void main(String[] args) {
 
--- service/src/test/java/com/avuar1/repository/CarRepositoryIT.java
+++ service/src/test/java/com/avuar1/repository/CarRepositoryIT.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 import static java.util.stream.Collectors.toList;
 import lombok.RequiredArgsConstructor;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
